bc4899be52040372861b3d79350c4e27d0cb4861
Saurabh Shukla <saurabh.shukla244@gmail.com>
2018-06-16 15:41:40 +0530

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,8 @@
+
+void dfs(int s) {
+        visited[s] = true;
+        for(int i = 0;i < adj[s].size();++i)    {
+         if(visited[adj[s][i]] == false)
+             dfs(adj[s][i]);
+        }
+    }
