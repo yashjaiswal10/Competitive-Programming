8bc564d5dcd94fe604d89164fc0e5579c49a2ec5
Saurabh Shukla <saurabh.shukla244@gmail.com>
2018-06-17 13:52:15 +0530

    Create template.cpp

---
+++ C- Template/template.cpp
@@ -0,0 +1,33 @@
+//############################################################################################
+//########################    Author:S1gma    ################################################
+//############################################################################################
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define umap unordered_map
+#define sz(a) int((a).size()) 
+#define rep(i,k,n) for (int i = k; i < n; ++i)
+#define repr(i,k,n) for (int i = k; i >= n; --i)
+#define all(c) (c).begin(),(c).end() 
+#define test long t;cin>>t;while(t--)
+#define fastIO ios_base::sync_with_stdio(0);cin.tie(0)
+#define mod 1000000007
+#define endl '\n'
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+
+
+int main() {
+    fastIO;
+    test{
+          
+    }
+	return 0;
+}
