08d4b850efa432f4d22510a9af5225f699744897
Saurabh Shukla <saurabh.shukla244@gmail.com>
2018-09-17 18:03:20 +0530

    updated

--- cpp-template/template.cpp
+++ cpp-template/template.cpp
@@ -1,33 +1,34 @@
-//############################################################################################
-//########################    Author:S1gma    ################################################
-//############################################################################################
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long 
 #define pb push_back
 #define mp make_pair
+#define endl '\n'
 #define ff first
 #define ss second
+#define mod 1000000007
+#define pq priority_queue
 #define umap unordered_map
 #define sz(a) int((a).size()) 
+#define clr(a,x) memset(a,x,sizeof(a))
 #define rep(i,k,n) for (int i = k; i < n; ++i)
 #define repr(i,k,n) for (int i = k; i >= n; --i)
-#define all(c) (c).begin(),(c).end() 
-#define test long t;cin>>t;while(t--)
+#define all(c) (c).begin(),(c).end()
+#define test() ll t;cin>>t;while(t--)
 #define fastIO ios_base::sync_with_stdio(0);cin.tie(0)
-#define mod 1000000007
-#define endl '\n'
+#define input() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout)
+
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int,int> pii;
 
 
+
 int main() {
     fastIO;
-    test{
-          
+    test(){
+
     }
-	return 0;
+    return 0;
 }
