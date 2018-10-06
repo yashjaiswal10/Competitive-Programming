e0dd71a86e2fc9a905a46871afef07505fe49a4b
Saurabh Shukla <saurabh.shukla244@gmail.com>
2018-10-06 16:43:32 +0530

    Update template.cpp

--- cpp-template/template.cpp
+++ cpp-template/template.cpp
@@ -10,13 +10,14 @@ using namespace std;
 #define mod 1000000007
 #define pq priority_queue
 #define umap unordered_map
-#define sz(a) int((a).size()) 
+#define sz(a) int((a).size())
 #define clr(a,x) memset(a,x,sizeof(a))
 #define rep(i,k,n) for (int i = k; i < n; ++i)
 #define repr(i,k,n) for (int i = k; i >= n; --i)
 #define all(c) (c).begin(),(c).end()
 #define test() ll t;cin>>t;while(t--)
 #define fastIO ios_base::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << (#x) << " is " << (x) << endl
 
 typedef vector<int> vi;
 typedef vector<ll> vll;
