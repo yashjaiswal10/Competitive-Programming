75bbed87757ab3845fc17cdfc5d498fa4b281131
Saurabh Shukla <saurabh.shukla244@gmail.com>
2019-03-03 19:39:44 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long 
+ 
+#define ff first
+#define ss second
 #define pb push_back
 #define mp make_pair
 #define endl "\n"
-#define ff first
-#define ss second
+#define ll long long
+#define ld long double
 #define l_b lower_bound
 #define u_b upper_bound
-#define pq priority_queue
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define heap priority_queue
 #define umap unordered_map
 #define sz(a) int((a).size())
-#define all(c) (c).begin(),(c).end()
+#define all(c) c.begin(),c.end()
 #define test() ll t;cin>>t;while(t--)
-#define clr(a,x) memset(a,x,sizeof(a))
-#define rep(i,n) for (int i = 0; i < n; ++i)
-#define repp(i,a,n) for (int i = a; i <= n; ++i)
-#define precise(x) cout<<fixed<<setprecision(x)
-#define debug(x) cout<<(#x)<<" is " <<(x)<<endl
+#define mset(a,x) memset(a,x,sizeof(a))
+#define rep(i,a,n) for(ll i = a; i < n; ++i) 
+#define debug(x) cout<<(#x)<<" is "<<(x)<<endl
 #define fastIO ios_base::sync_with_stdio(0);cin.tie(0)
-
-
-using ld = long double;
-using vi = vector<int>;
-using vll = vector<ll>;
-using pii = pair<int,int>;
-using pll = pair<ll, ll>;
-using vpii = vector<pii>;
-using vpll = vector<pll>;
-
-const int mod=1e9+7;
-const int maxn=1e6+5;
-ll a[maxn];
-
+ 
+const int inf = 1e9;
+const int mod = 1e9 + 7;
+const int N = 1e6 + 5;
+ 
+ 
 int main() {
-    //fastIO;
-    test(){
-
-    }
+	// fastIO;
+    
+    
     return 0;
 }
